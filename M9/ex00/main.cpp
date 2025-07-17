db42fab7e6129a210493dc2582007c13341f6940
sparklebutt <arandomsanti@gmail.com>
2025-07-17 18:29:55 +0300

    chore:: cleanup

--- M9/ex00/main.cpp
+++ M9/ex00/main.cpp
@@ -3,26 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shaboom <shaboom@student.42.fr>            +#+  +:+       +#+        */
+/*   By: araveala <araveala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 11:57:21 by shaboom           #+#    #+#             */
-/*   Updated: 2025/04/16 21:03:01 by shaboom          ###   ########.fr       */
+/*   Updated: 2025/07/17 18:26:32 by araveala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main ()
+int main (int argc, char **argv)
 {
-	try
-	{
-		BitcoinExchange test("cpp_09/data.csv", "test.txt");
-		test.showValues();
-		/* code */
-		
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <conversions_file>" << std::endl;
+		return 1;
 	}
-	catch(const std::exception& e)
-	{
+	
+	std::string ratesFile = "cpp_09/data.csv"; // Default rates file
+	std::string conversionsFile = argv[1];
+	if (ratesFile.empty() || conversionsFile.empty()) {
+		std::cerr << "Error: File names cannot be empty." << std::endl;
+		return 1;
+	}
+	try {
+		BitcoinExchange test(ratesFile, conversionsFile);
+		test.showValues();
+	} catch(const std::exception& e){
 		std::cerr << e.what() << '\n';
 	}
 	
